c428f72694e8856ed2ccc0f3c6b5e765acb794df
git-demo <3493281952@qq.com>
2025-01-18 15:24:07 +0800

    初始化项目

---
+++ src/store/getters.js
@@ -0,0 +1,9 @@
+const getters = {
+  sidebar: state => state.app.sidebar,
+  device: state => state.app.device,
+  token: state => state.user.token,
+  avatar: state => state.user.avatar,
+  name: state => state.user.name
+}
+// getters方便属性快捷访问
+export default getters
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+    <!-- 引入图标 -->
+    <svg-icon icon-class="dashboard" />
+    <svg-icon icon-class="example" />
+    <svg-icon icon-class="eye-open" />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
