f999d2f1efac46d5bdf6c60ab63d4b417a3cdc4b
git-demo <3493281952@qq.com>
2025-01-19 22:40:09 +0800

    axios测试

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -20,6 +20,7 @@
           <el-form-item>
             <el-button style="width:350px" type="primary" block @click="login">登录</el-button>
           </el-form-item>
+          <el-button @click="textAxios">测试axios封装</el-button>
         </el-form>
       </el-card>
     </div>
@@ -27,6 +28,7 @@
 </template>
 
 <script>
+import request from '@/utils/request'
 export default {
   name: 'Login',
   data() {
@@ -77,6 +79,16 @@ export default {
           // 登录
           // this.$store.dispatch('user/login', this.loginForm)
           alert('校验通过')
+        }
+      })
+    },
+    textAxios() {
+      request({
+        url: '/sys/login',
+        method: 'POST',
+        data: {
+          mobile: '13912345678',
+          password: '123456'
         }
       })
     }
