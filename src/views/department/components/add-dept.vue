f15dee0bb11bb23ebb6a5c57123f15a2a94e9279
git-demo <3493281952@qq.com>
2025-01-24 20:17:12 +0800

    表单添加的校验

---
+++ src/views/department/components/add-dept.vue
@@ -0,0 +1,102 @@
+<template>
+  <el-dialog title="新增部门" :visible="showDialog" @close="close">
+    <!-- 放置弹窗内容 -->
+    <el-form ref="addDept" :model="formData" :rules="rules" label-width="120px">
+      <el-form-item prop="name" label="部门名称">
+        <el-input v-model="formData.name" placeholder="2-10个字符" style="width: 80%" size="mini" />
+      </el-form-item>
+
+      <el-form-item prop="code" label="部门编码">
+        <el-input v-model="formData.code" placeholder="2-10个字符" style="width: 80%" size="mini" />
+      </el-form-item>
+
+      <el-form-item prop="managerId" label="部门负责人">
+        <el-select v-model="formData.managerId" placeholder="请选择负责人" style="width: 80%" size="mini" />
+      </el-form-item>
+
+      <el-form-item prop="introduce" label="部门介绍">
+        <el-input v-model="formData.introduce" placeholder="1-100个字符" type="textarea" size="mini" :rows="4" style="width: 80%" />
+      </el-form-item>
+
+      <el-form-item>
+        <el-row type="flex" justify="center">
+          <el-col :span="12">
+            <el-button size="mini" type="primary">确定</el-button>
+            <el-button size="mini">取消</el-button>
+          </el-col>
+        </el-row>
+      </el-form-item>
+
+    </el-form>
+  </el-dialog>
+</template>
+
+<script>
+import { getDepartment } from '@/api/department'
+import { call } from 'body-parser'
+export default {
+  props: {
+    showDialog: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      formData: {
+        code: '',
+        introduce: '',
+        managerId: '',
+        name: '',
+        pid: ''
+      },
+      rules: {
+        code: [{ required: true, message: '部门编码不能为空', trigger: 'blur' },
+          { min: 2, max: 10, message: '部门编码的长度为2-10个字符', trigger: 'blur' },
+          {
+            trigger: 'blur',
+            validator: async(rule, value, callback) => {
+              // value 就是输入的编码
+              const result = await getDepartment()
+              // 检验result数组中是否存在 value 值
+              if (result.some(item => item.code === value)) {
+                callback(new Error('部门中已有此编码'))
+              } else {
+                callback()
+              }
+            }
+          }
+        ],
+        introduce: [{ required: true, message: '部门介绍不能为空', trigger: 'blur' }, {
+          min: 1, max: 100, message: '部门介绍的长度为1-100个字符', trigger: 'blur' }], // 部门介绍
+        managerId: [{ required: true, message: '部门负责人不能为空', trigger: 'blur' }], // 部门负责人id
+        name: [{ required: true, message: '部门名称不能为空', trigger: 'blur' },
+          { min: 2, max: 10, message: '部门名称的长度为2-10个字符', trigger: 'blur' },
+          {
+            trigger: 'blur',
+            validator: async(rule, value, callback) => {
+              // value 就是输入的编码
+              const result = await getDepartment()
+              // 检验result数组中是否存在 value 值
+              if (result.some(item => item.name === value)) {
+                callback(new Error('部门中已有此名称'))
+              } else {
+                callback()
+              }
+            }
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    close() {
+      // 修改父组件的值 子传父
+      this.$emit('update:showDialog', false)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -2,39 +2,43 @@
   <div class="container">
     <div class="app-container">
       <!-- 树形结构 -->
-      <el-tree default-expand-all :data="depts" :props="defaultProps">
+      <el-tree :expand-on-click-node="false" default-expand-all :data="depts" :props="defaultProps">
         <!-- 节点结构 -->
         <template v-slot="{ data }">
           <el-row style="width: 100%;height: 40px;" type="flex" justify="space-between" align="middle">
             <el-col>{{ data.name }}</el-col>
             <el-col :span="4">
               <span class="tree-manager">{{ data.managerName }}</span>
-              <el-dropdown>
+              <el-dropdown @command="operateDept">
                 <span class="el-dropdown-link">
                   操作<i class="el-icon-arow-doen el-icon--ringt" />
                 </span>
                 <el-dropdown-menu slot="dropdown">
-                  <el-dropdown-item>添加子部门</el-dropdown-item>
-                  <el-dropdown-item>编辑子部门</el-dropdown-item>
-                  <el-dropdown-item>删除子部门</el-dropdown-item>
+                  <el-dropdown-item command="add">添加子部门</el-dropdown-item>
+                  <el-dropdown-item command="edit">编辑子部门</el-dropdown-item>
+                  <el-dropdown-item command="del">删除子部门</el-dropdown-item>
                 </el-dropdown-menu>
               </el-dropdown>
             </el-col>
           </el-row>
         </template>
-
       </el-tree>
     </div>
+    <!-- 放置弹层 -->
+    <add-dept :show-dialog.sync="showDialog" />
   </div>
 </template>
 
 <script >
 import { getDepartment } from '@/api/department'
 import { transListToTreeData } from '@/utils'
+import AddDept from './components/add-dept.vue'
 export default {
   name: 'Department',
+  components: { AddDept },
   data() {
     return {
+      showDialog: false,
       depts: [],
       defaultProps: {
         label: 'name',
@@ -49,6 +53,12 @@ export default {
     async getDepartment() {
       const result = await getDepartment()
       this.depts = transListToTreeData(result, 0)
+    },
+    operateDept(type) {
+      if (type === 'add') {
+        // 添加子部门
+        this.showDialog = true
+      }
     }
   }
 }
