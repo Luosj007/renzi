0d50794cef19d05a8ff2698ccb00fa65c413f3eb
git-demo <3493281952@qq.com>
2025-01-24 12:10:20 +0800

    作用域插槽

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -1,7 +1,29 @@
 <template>
   <div class="container">
     <div class="app-container">
-      <el-tree default-expand-all :data="depts" :props="defaultProps" />
+      <!-- 树形结构 -->
+      <el-tree default-expand-all :data="depts" :props="defaultProps">
+        <!-- 节点结构 -->
+        <template v-slot="{ data }">
+          <el-row style="width: 100%;height: 40px;" type="flex" justify="space-between" align="middle">
+            <el-col>{{ data.name }}</el-col>
+            <el-col :span="4">
+              <span class="tree-manager">{{ data.managerName }}</span>
+              <el-dropdown>
+                <span class="el-dropdown-link">
+                  操作<i class="el-icon-arow-doen el-icon--ringt" />
+                </span>
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item>添加子部门</el-dropdown-item>
+                  <el-dropdown-item>编辑子部门</el-dropdown-item>
+                  <el-dropdown-item>删除子部门</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </el-col>
+          </el-row>
+        </template>
+
+      </el-tree>
     </div>
   </div>
 </template>
@@ -11,12 +33,15 @@ export default {
   name: 'Department',
   data() {
     return {
-      depts: [{ name: '传智教育', children: [{
-        name: '总裁办'
+      depts: [{ name: '传智教育', managerName: '管理员', children: [{
+        name: '总裁办',
+        managerName: '张三'
       }, {
-        name: '行政部'
+        name: '行政部',
+        managerName: '李四'
       }, {
-        name: '人事部'
+        name: '人事部',
+        managerName: '王五'
       }] }],
       defaultProps: {
         label: 'name',
@@ -31,5 +56,10 @@ export default {
 .app-container{
   padding: 30px 140px;
   font-size: 14px;
+}
+.tree-manager{
+  width: 50px;
+  display: inline-block;
+  margin: 10px;
 }
 </style>
